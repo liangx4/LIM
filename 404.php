6047251fcf54a912e24537348d389a5d8a214eff
liangx4 <xiao.polarr@gmail.com>
2015-08-28 14:05:46 -0700

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,9 @@
+
+<?php get_header(); ?>
+
+<h1>Page not found</h1>
+<p>Read <a href="/">the most recent post</a>, or select one of the articles from the list below.</p>
+
+<?php include 'allposts.php'; ?>
+
+<?php get_footer(); ?> 
