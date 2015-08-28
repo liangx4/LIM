e2a0e4ddea068c188d9a0e78013cfd329addb07b
liangx4 <xiao.polarr@gmail.com>
2015-08-28 14:07:43 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<ul class="f">
+<br/>
+<br/>
+<?php comments_template(); ?> 
+<br/>
+
+	<p>
+	<a href="/">Homepage</a> :: <a href="/about-me/">About me</a> :: <a href="/archives/">Archives</a> :: <a href="/neighbors/">Neighbors</a> 
+	</p>
+<br/>
+<li>Xiao Liang &copy; 2014</li>
+</ul>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
